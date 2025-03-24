1de18d2d4230666433639ea698b9ba03030a5b5d
singyuKang <rkdtlscks123@naver.com>
2025-03-24 20:59:33 +0900

    feat : 기본 라우팅 구현

--- src/main.js
+++ src/main.js
@@ -233,9 +233,18 @@ const ProfilePage = () => `
   </div>
 `;
 
-document.body.innerHTML = `
-  ${MainPage()}
-  ${ProfilePage()}
-  ${LoginPage()}
-  ${ErrorPage()}
-`;
+const routePage = {
+  "/": MainPage,
+  "/login": LoginPage,
+  "/profile": ProfilePage,
+};
+
+// 페이지 렌더링 함수
+function renderPage() {
+  const path = window.location.pathname;
+  const root = document.getElementById("root");
+  const page = routePage[path] || ErrorPage;
+  root.innerHTML = page();
+}
+
+renderPage();
