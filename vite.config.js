bd0cc82dbdc6e84ac8792343594166ec6a637f24
singyuKang <rkdtlscks123@naver.com>
2025-03-26 19:51:26 +0900

    fix : vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  base: "/",
+  base: "/front_5th_chapter1-1/",
   test: {
     globals: true,
     environment: "jsdom",
