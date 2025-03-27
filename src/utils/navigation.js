7bad8f17d6789ec06b481c7a17ea826aaf259191
singyuKang <rkdtlscks123@naver.com>
2025-03-27 09:44:23 +0900

    fix : baseURL 라우팅 버그 처리

--- src/utils/navigation.js
+++ src/utils/navigation.js
@@ -1,12 +1,15 @@
 import { renderPage } from "../main";
 
+const BASE_URL = import.meta.env.BASE_URL || "/";
+
 export function navigate(url) {
   if (window.location.hash) {
     // 해시 라우터
     window.location.hash = url;
   } else {
     // 브라우저 라우터
-    window.history.pushState({}, "", url);
+    const newUrl = BASE_URL + url.replace(/^\//, "");
+    window.history.pushState({}, "", newUrl);
     renderPage();
   }
 }
